e9ba7d1d282298b9dba55edb57c04110a0b21c7f
Nitish Rathi <nitish@redpanda.co.in>
2015-04-28 14:22:32 +0530

    fix variable naming

--- flask_admin/static/admin/js/form-1.0.0.js
+++ flask_admin/static/admin/js/form-1.0.0.js
@@ -158,8 +158,8 @@
         }
 
         // set up tiles
-        mapbox_version = window.MAPBOX_ACCESS_TOKEN ? 4 : 3;
-        L.tileLayer('http://{s}.tiles.mapbox.com/v'+mapbox_version+'/'+MAPBOX_MAP_ID+'/{z}/{x}/{y}.png?access_token=' + window.MAPBOX_ACCESS_TOKEN, {
+        mapboxVersion = window.MAPBOX_ACCESS_TOKEN ? 4 : 3;
+        L.tileLayer('http://{s}.tiles.mapbox.com/v'+mapboxVersion+'/'+MAPBOX_MAP_ID+'/{z}/{x}/{y}.png?access_token=' + window.MAPBOX_ACCESS_TOKEN, {
           attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
           maxZoom: 18
         }).addTo(map);
